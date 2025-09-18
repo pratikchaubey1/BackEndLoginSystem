37ef7899b4b43fe42d8cb703471f4708e6f7146a
Pratik Chaubey <pratikup98@gmail.com>
2025-09-18 22:05:33 +0530

    just for try

--- Index.js
+++ Index.js
@@ -1,4 +1,35 @@
+// const express = require('express')
+// const cookieParser = require('cookie-parser')
+
+// const app = express()
+// require('dotenv').config()
+
+// const User = require('./routes/User')
+// const admin = require('./routes/adminlogin')
+// const port = process.env.PORT || 3000
+
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
+// app.use(cookieParser())
+
+// app.get('/',(req,res) => {
+//   res.send('Home page routes ')
+// })
+// const todoRoutes = require('./routes/todo')
+
+// app.use('/api/v1',todoRoutes)
+// app.use('/autho',User);
+// app.use('/admin',admin);
+
+// app.listen(port,()=>{
+//   console.log(`server on listen port no:${port}`);
+  
+// })
+
+// const dbconnect = require('./Config/database')
+// dbconnect()
 const express = require('express')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 require('dotenv').config()
@@ -8,6 +39,8 @@ const admin = require('./routes/adminlogin')
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 app.get('/',(req,res) => {
   res.send('Home page routes ')
@@ -24,4 +57,4 @@ app.listen(port,()=>{
 })
 
 const dbconnect = require('./Config/database')
-dbconnect()
+dbconnect() 
